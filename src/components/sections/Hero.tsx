c8afc2639bbc1c36399057b90b301916ce33de9d
Mohana Y <166400745+Mohaanaaaa@users.noreply.github.com>
2025-01-09 13:18:17 +0530

    Update Hero.tsx

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -38,7 +38,7 @@ export function Hero() {
           className="w-32 h-32 mx-auto mb-6 pixelated pixel-border"
         />
       </div>
-      <h1 className="text-4xl mb-4 text-slate-800" style={{lineHeight:'1.6'}}> DEV &gt;&gt; Mohana_Y</h1>
+      <h1 className="text-4xl mb-4 text-slate-800" style={{lineHeight:'1.6'}}> DEV &gt;&gt; Mohanaa</h1>
       <p className="text-lg text-slate-600" style={{lineHeight:'2.5'}}>LEVEL 07 FRONTEND & BACKEND WIZARD</p>
 
       {/* Icon Section */}
